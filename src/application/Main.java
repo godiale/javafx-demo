d7311ebaf387000ec264f0173a37da8a18676f36
godiale <godin_a_e@mail.ru>
2017-03-18 21:48:55 +0300

    Multiple scenes supported (started)
    -- Second scene is just a mockup

--- src/application/Main.java
+++ src/application/Main.java
@@ -19,57 +19,84 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    @SuppressWarnings("static-access")
+
+    Scene sceneName, sceneMove;
+
     @Override
     public void start(Stage primaryStage) {
         try {
             primaryStage.setTitle("Sea Battle");
-            GridPane grid = new GridPane();
-            grid.setAlignment(Pos.CENTER);
-            grid.setHgap(10);
-            grid.setVgap(10);
-            grid.setPadding(new Insets(25, 25, 25, 25));
-
-            Text scenetitle = new Text("Welcome to Sea Battle");
-            scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-            grid.add(scenetitle, 0, 0, 2, 1);
-
-            Label playerName = new Label("User Name:");
-            grid.add(playerName, 0, 1);
-
-            TextField playerTextField = new TextField();
-            grid.add(playerTextField, 1, 1);
-
-            Button btn = new Button("Next");
-            HBox hbBtn = new HBox(10);
-            hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-            hbBtn.getChildren().add(btn);
-            grid.add(hbBtn, 1, 4);
-
-            final Text actiontarget = new Text();
-            grid.add(actiontarget, 0, 6);
-            grid.setColumnSpan(actiontarget, 2);
-            grid.setHalignment(actiontarget, HPos.RIGHT);
-            actiontarget.setId("actiontarget");
-
-            Group group = new Group();
-            group.getChildren().addAll(grid);
-
-            Scene scene = new Scene(group, 300, 275);
-            primaryStage.setScene(scene);
+            createSceneName(primaryStage);
+            createSceneMove(primaryStage);
+            primaryStage.setScene(sceneName);
             primaryStage.show();
-
-            btn.setOnAction(new EventHandler<ActionEvent>() {
-
-                @Override
-                public void handle(ActionEvent e) {
-                    group.getChildren().clear();
-                    System.out.println("Done");
-                }
-            });
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @SuppressWarnings("static-access")
+    private void createSceneName(Stage primaryStage) {
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Welcome to Sea Battle");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label playerName = new Label("User Name:");
+        grid.add(playerName, 0, 1);
+
+        TextField playerTextField = new TextField();
+        grid.add(playerTextField, 1, 1);
+
+        Button btn = new Button("Next");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 0, 6);
+        grid.setColumnSpan(actiontarget, 2);
+        grid.setHalignment(actiontarget, HPos.RIGHT);
+        actiontarget.setId("actiontarget");
+
+        Group group = new Group();
+        group.getChildren().addAll(grid);
+
+        sceneName = new Scene(group, 300, 275);
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                System.out.println("My name is " + playerTextField.getText());
+                primaryStage.setScene(sceneMove);
+            }
+        });
+    }
+
+    private void createSceneMove(Stage primaryStage) {
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("BUBU");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Group group = new Group();
+        group.getChildren().addAll(grid);
+
+        sceneMove = new Scene(group, 300, 275);
     }
 
     public static void main(String[] args) {
